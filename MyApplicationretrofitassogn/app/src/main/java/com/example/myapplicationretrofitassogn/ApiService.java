e1b009b58a43201b9ec0bf6c083269d646c46a51
loganrv10 <subhamsingh5338@gmail.com>
2021-07-15 22:09:53 +0530

    retrofit

---
+++ MyApplicationretrofitassogn/app/src/main/java/com/example/myapplicationretrofitassogn/ApiService.java
@@ -0,0 +1,11 @@
+package com.example.myapplicationretrofitassogn;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+
+public interface ApiService {
+    @GET("/api/unknown/{userId}")
+    Call<ResponseModel> getUser(@Path("userId") int userId);
+}
