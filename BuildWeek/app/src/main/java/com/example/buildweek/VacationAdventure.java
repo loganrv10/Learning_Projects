8f7eb7ee70fabc2082da86c60c977431d85f3252
loganrv10 <subhamsingh5338@gmail.com>
2021-07-22 09:59:19 +0530

    Quiz for Happn app

---
+++ BuildWeek/app/src/main/java/com/example/buildweek/VacationAdventure.java
@@ -0,0 +1,14 @@
+package com.example.buildweek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VacationAdventure extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vacation_main);
+    }
+}
