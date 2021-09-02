2649080b92c12e47705e8608c41dbc046080ac48
Subham kumar <80710806+loganrv10@users.noreply.github.com>
2021-09-03 00:32:26 +0530

    2 fragments 1 activity

---
+++ src/main/java/com/example/a1activity2fragments/fragments_2.java
@@ -0,0 +1,21 @@
+package com.example.a1activity2fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class fragments_2 extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragments_2, container, false);
+    }
+}
